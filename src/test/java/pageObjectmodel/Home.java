c412fc7c7f1ac47a9efb7be1550b4fe6331c6b7e
Rudra Prasad <rudraprasadmallik46@gmail.com>
2024-12-19 16:37:01 +0530

    HomeTest error  fixes.

--- src/test/java/pageObjectmodel/Home.java
+++ src/test/java/pageObjectmodel/Home.java
@@ -1,5 +1,6 @@
 package pageObjectmodel;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -24,13 +25,63 @@ public class Home {
 	        System.out.println("Browser window maximized");
 	    }
 	 
-//	 public void winlogoHome() {
-//		 String logoLocator = BaseTest.loc.getProperty("home-logo-url"); 
-//		 
-//		 WebElement logo = driver.findElement("home-logo-url");
-//		 
-//	 }
-	 
+        public WebElement winlogoHome() {
+     		 String logoLocator = BaseTest.loc.getProperty("home-logo-link"); 
+		 
+     		 WebElement logo = driver.findElement(By.cssSelector(logoLocator));
+			return logo ;		
+     		
+     	 }	
+        
+        // Tabs
+        
+        public By homeTab() {
+        WebElement homeTab= driver.findElement(By.xpath("home-link"));
+        	
+			return (By) homeTab;
+        }
+        
+        public WebElement tvShowsTab() {
+            WebElement tvshowsTab= driver.findElement(By.xpath("tvShows-link"));
+            	
+    			return tvshowsTab;
+            }
+        
+        public WebElement moviesTab() {
+            WebElement moviesTab= driver.findElement(By.xpath("movies-linlk"));
+            	
+    			return moviesTab;
+            }
+        
+        public WebElement watchFreeTab() {
+            WebElement watchfreeTab= driver.findElement(By.xpath("watchFree-link"));
+            	
+    			return watchfreeTab;
+            }
+        public WebElement liveTvTab() {
+            WebElement livetvTab= driver.findElement(By.xpath("livetv-link"));
+            	
+    			return livetvTab;
+            }
+        
+        //Buttons on navbar
+        
+        public WebElement searchButton() {
+            WebElement searchButton= driver.findElement(By.xpath("searchbtn-link"));
+            	
+    			return searchButton;
+            }
+        
+        public WebElement subscribeButton() {
+            WebElement subscribebutton = driver.findElement(By.xpath("subscribebtn-link"));
+            	
+    			return subscribebutton;
+            }
+        public WebElement loginButton() {
+            WebElement loginbutton = driver.findElement(By.xpath("loginbtn-link"));
+            	
+    			return loginbutton;
+            }
 	 
 	 
 
--- src/test/java/testcase/HomeTest.java
+++ src/test/java/testcase/HomeTest.java
@@ -1,5 +1,14 @@
 package testcase;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -30,17 +39,55 @@ public class HomeTest extends BaseTest{
 			System.out.println("This Url is not matching");
 		}
 		
+		homePage.maximizeScreen();
 	}
 	
 	
+	
+	@Test(priority =2)
 	public void verifyHomeWinLogo() {
-		
-		
+		 WebElement logo = homePage.winlogoHome();
+		 
+		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
+		    wait.until(ExpectedConditions.visibilityOf(logo));
+	    
+	    Assert.assertTrue(logo.isDisplayed(), "Home logo is not displaying.");
+	    
+	    org.openqa.selenium.Point position = logo.getLocation();
+	    int x = position.getX();
+	    int y = position.getY();
+	    System.out.println("Logo is located at X: " + x + ", Y: " + y);
+	    Assert.assertTrue(x >= 0, "Logo X position is invalid.");
+	    Assert.assertTrue(y >= 0, "Logo Y position is invalid.");
 	}
+
+
 	
+	@Test(priority =3)
+	public void allicontentVisibilityonHome() {
+		
+		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	        // List all the locators for the elements you want to check
+		 List<By> locators = Arrays.asList(
+		            homePage.homeTab()
+//		            homePage.tvShowsTab(),
+//		            homePage.moviesTab(),
+//		            homePage.watchFreeTab(),
+//		            homePage.liveTvTab(),
+//		            homePage.searchButton(),
+//		            homePage.subscribeButton(),
+//		            homePage.loginButton()
+		    );
+
+		    // Iterate over the list of locators and check their visibility
+		    for (By locator : locators) {
+		        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));  // Use By locators here
+		        Assert.assertNotNull(element, "Element is not found for locator: " + locator);
+		        Assert.assertTrue(element.isDisplayed(), "Element is not displayed for locator: " + locator);
+		    }
 	
-	
-	
+	}
 	
 	
 	
